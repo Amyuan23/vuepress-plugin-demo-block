e88762f784128d9edf716dc26664953996519c5a
jpfs <jpfs@gft.com>
2019-04-15 19:10:34 +0200

    fix: deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,9 @@ cd docs/.vuepress/dist
 
 git init
 git add -A
-git commit -m 'deploy docs'
+git commit -m 'deploy docs [skip ci]'
 
-git push -f git@github.com:d0whc3r/vuepress-plugin-demo-block.git master:gh-pages
+git push -f https://github.com/d0whc3r/vuepress-plugin-demo-block.git master:gh-pages
 
 cd -
 rm -rf docs/.vuepress/dist

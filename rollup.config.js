d633c7dee4badf666922020fa1e6c61e3fc08035
jpfs <jpfs@gft.com>
2019-04-15 19:03:28 +0200

    fix: vuepress and rollup versions

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from "rollup-plugin-babel";
 import common from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
 import postcss from "rollup-plugin-postcss";
-import string from 'rollup-plugin-string';
+import { string } from "rollup-plugin-string";
 
 export default [
   {
